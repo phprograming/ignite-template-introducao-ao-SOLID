6f526e26941c28518cec093118f8a81bbc516303
unknown <paulo_hbs17@hotmail.com>
2022-12-23 17:56:38 -0300

    docs: Added documentation with swagger.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
 
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
+import swaggerFile from './swagger.json';
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use("/users", usersRoutes);
 
 export { app };
